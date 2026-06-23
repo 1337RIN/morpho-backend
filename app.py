051572866b34fada024f42f449421aeff9a5f211
1337RIN <dmtyurin16@mail.ru>
2026-06-23 19:16:34 +0300

    Fix port binding for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS # Импортируем CORS
 from main import analyze_sentence
@@ -15,4 +16,9 @@ def api_analyze():
     return jsonify(analysis_result)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Render автоматически передает порт в переменные окружения PORT.
+    # Если мы запускаем локально, сработает дефолтное значение 5000.
+    port = int(os.environ.get("PORT", 5000))
+    
+    # На Render крайне важно слушать хост 0.0.0.0, чтобы сервер принимал внешние запросы
+    app.run(host='0.0.0.0', port=port, debug=False)
